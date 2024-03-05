5bd6ca8613d48661df81e9e4fcec1ac08fe66064
David Hof <d.hof@umail.leidenuniv.nl>
2024-03-05 19:34:56 +0100

    Added new folder with code for local sleep project

---
+++ local_sleep/visualize_wave_pars.m
@@ -0,0 +1,45 @@
+function [] = visualize_wave_pars(wave_pars)
+
+field_names = fieldnames(wave_pars);
+
+for i = 1:length(field_names)
+    field_name = field_names{i};
+    if strcmp(field_name, 'wvspermin')
+        plot_title = 'Slow-Wave Density';
+        cb_label = 'min^-^1';
+        par_name = 'density';
+    elseif strcmp(field_name, 'p2pamp')
+        plot_title = 'Peak-To-Peak Amplitude';
+        cb_label = 'μV';
+        par_name = 'p2pamp';
+    elseif strcmp(field_name, 'dslope')
+        plot_title = 'Downward Slope';
+        cb_label = 'μV.s^-^1';
+        par_name = 'dslope';
+    else
+        plot_title = 'Upward Slope';
+        cb_label = 'μV.s^-^1';
+        par_name = 'uslope';
+    end
+  
+    topoplot(wvspermin, EEG.chanlocs, 'style', 'both', 'shading', 'interp', 'plotrad', 0.85, 'headrad', 0.84);
+    colormap(parula);
+    clim([floor(min(wave_par)) ceil(max(wave_par))]);
+    title(plot_title,'FontSize', 18);
+    cb = colorbar;
+    cb.FontSize = 12;
+    cb.Label.String = cb_label;
+    cb.Label.FontSize = 18;
+    cb.Label.Rotation = 270;
+    cb.Label.Position = [4.5 cb.Label.Position(2) cb.Label.Position(3)];
+    patch = findobj(gcf, 'Type', 'patch');
+    set(patch, 'FaceColor', 'white', 'EdgeColor', 'black', 'EdgeAlpha', 0);
+    lines = findobj(gcf, 'Type', 'line');
+    set(lines(5), 'LineWidth', 3);
+    set(lines(2:4), 'LineWidth', 1.5);
+    set(lines(1), 'MarkerSize', 5);
+    filename = sprintf('topoplot_%s.png', par_name);
+    saveas(gcf, filename);
+end
+
+end
