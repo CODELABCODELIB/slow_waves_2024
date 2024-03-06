d41e7bf09fa0c774a1d09fe7f6928ed34afd9097
David Hof <d.hof@umail.leidenuniv.nl>
2024-03-06 20:11:39 +0100

    updated test_processing4 and associated slurm files

--- local_sleep/test_processing4.m
+++ local_sleep/test_processing4.m
@@ -1,13 +1,30 @@
-function [processed_EEG] = test_processing4(EEG, ref_electrodes)
+function [processed_EEG] = test_processing4(EEG, varargin)
 
-% Check if ref_electrodes argument is passed
-if nargin < 2
-    % If ref_electrodes are not passed, default to [52 58]
-    ref_electrodes = [52 58];
+% Arguments: 'EEG', 'data_part' (optional; 'movie'/'phone'), 'ref_electrodes' (optional)
+
+% Initialize default for 'ref_electrodes'
+ref_electrodes = [52 58];
+
+% Parse varargin for 'data_part' and 'ref_electrodes'
+if ~isempty(varargin)
+    for i = 1:length(varargin)
+        if strcmpi(varargin{i}, 'data_part')
+            data_part = varargin{i+1};
+        elseif strcmpi(varargin{i}, 'ref_electrodes')
+            ref_electrodes = varargin{i+1};
+        end
+    end
 end
 
-% Initialize struct array for processed EEG data
-processed_EEG = struct();
+% Process EEG data based on 'data_part'
+if exist('data_part', 'var')
+    switch data_part
+        case 'movie'
+            EEG = pop_select(EEG, 'point', [1 4231882]); % Select movie part of EEG data
+        case 'phone'
+            EEG = pop_select(EEG, 'point', [4231882 7101108]); % % Select phone part of EEG data
+    end
+end
 
 % Apply high-pass filter >0.1 Hz using a two-pass fifth-order Butterworth filter
 [bf, af] = butter(5, 0.1/(EEG.srate/2), 'high');
@@ -24,12 +41,6 @@ EEG = pop_reref(EEG, ref_electrodes, 'keepref', 'on');
 % Band-pass filter the signal in the delta band (1-4 Hz)
 EEG = pop_eegfiltnew(EEG, 1, 4);
 
-% Select movie/phone parts of the EEG data
-Movie = pop_select(EEG, 'point', [1 4231882]);
-Phone = pop_select(EEG, 'point', [4231882 7101108]);
-
-processed_EEG.Complete = EEG;
-processed_EEG.Movie = Movie;
-processed_EEG.Phone = Phone;
+processed_EEG = EEG;
 
 end
