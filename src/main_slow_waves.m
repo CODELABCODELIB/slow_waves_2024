4caaff2a7d9ca81a829b43f7382c65ec734d3c81
Ruchella Kock <ruchella_kock@hotmail.com>
2024-09-24 15:41:16 +0200

    remove behavior_to_sw code

--- src/main_slow_waves.m
+++ src/main_slow_waves.m
@@ -26,38 +26,7 @@ unique_name = 'sw_test'; bandpass_lower = 1; bandpass_upper = 4;
 % run f using f2
 f = @sw_detection; 
 f2 = @call_f_all_p_parallel_sw; 
-gen_checkpoints(unique_name,bandpass_lower,bandpass_upper, f,f2, 'processed_data_path',processed_data_path,'save_path_upper',save_path_upper, 'count',10);
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%% Figure 4:JID-waves NNMF %%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%% JID-amplitudes
-save_path = sprintf('%s/jid_amp',save_path_upper); 
-data_path = sprintf('%s/erp_sw2_1_4',save_path_upper);
-load_str='sw2'; data_name='A';
-if ~exist(save_path, 'dir')
-       mkdir(save_path); addpath(genpath(save_path))
-end
-f = @jid_waves_main;
-run_f_checkpoints(data_path,load_str,data_name,f, 'save_path', save_path, 'aggregate_res', 1);
-%% cluster the nnmf maps across the population
-data_path = sprintf('%s/jid_amp',save_path_upper);
-load(sprintf('%s/EEG_res.mat',data_path))
-% remove the empty structs and concat all the participants
-res = res(cellfun(@(x) isfield(x,'stable_basis_amp'),res));
-res = cat(2,res{:});
-% select the spatial maps
-all_maps = cat(2,res.stable_basis_amp);
-[prototypes,labels,cluster_prototypes,cluster_labels] = cluster(all_maps);
-%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%% Figure 3:JID-waves NNMF with delays %%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-f=@calculate_p2p_amplitude;
-n_timelags = [0:0.5:10];
-save_path = sprintf('%s/jid_amp_delay_nnmf_%d_to_%d',save_path_upper,min(n_timelags),max(n_timelags));
-if ~exist(save_path, 'dir')
-       mkdir(save_path); addpath(genpath(save_path))
-end
-jid_delay_nnmf_main(res,f, 'save_path',save_path,'n_timelags',n_timelags)
+gen_checkpoints(unique_name,bandpass_lower,bandpass_upper, f,f2, 'processed_data_path',processed_data_path,'save_path_upper',save_path_upper, 'count',18);
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%% Slow waves to behavior %%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
