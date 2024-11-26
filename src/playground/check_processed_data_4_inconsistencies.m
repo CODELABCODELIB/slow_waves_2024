6e70dd89ad79f69599bdc6fe8ed9ea11b7cdc6fd
Ruchella Kock <ruchella_kock@hotmail.com>
2024-11-26 14:27:18 +0100

    check inconsistencies in the data

---
+++ src/playground/check_processed_data_4_inconsistencies.m
@@ -0,0 +1,12 @@
+% load the file
+data_path = '/mnt/ZETA18/User_Specific_Data_Storage/ruchella/slow_waves/sws_processed/durations.txt';
+fieldnames = {'checkpoint','pp','phone','movie', 'phone_end', 'phone_start','movie_end', 'movie_start'};
+T = readtable(data_path);
+T.Properties.VariableNames = fieldnames;
+s = table2struct(T);
+[c,idx] = unique({s.pp});
+s = s(idx);
+%%
+phone_durations = round(([s.phone_end]-[s.phone_start])/60000);
+figure; histogram(phone_durations)
+figure; histogram(round([s.movie]))
