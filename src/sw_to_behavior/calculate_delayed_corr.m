1a2a05fbc6d40df197cf8ca083a2b1712c4d933c
Ruchella Kock <ruchella_kock@hotmail.com>
2024-11-27 14:21:02 +0100

    documentation sw correlational analysis with delay

--- src/sw_to_behavior/calculate_delayed_corr.m
+++ src/sw_to_behavior/calculate_delayed_corr.m
@@ -1,16 +1,41 @@
-function [rhos,ps] = calculate_delayed_corr(features)
+function [rhos,ps] = calculate_delayed_corr(features,options)
+%% calculate spearman correlation between slow wave features and rate
+%
+% **Usage:** [rhos,ps] = calculate_delayed_corr(features)
+%
+% Input(s):
+%   - features =  struct with amplitudes, density and rates (from prepare_features_rate.m) 
+%
+% Optional Input(s)
+%   - n_delays = minutes by which to shift the data;
+%   - n_chans = number of electrodes;
+%
+%
+% Output(s):
+%   - rhos = struct with spearman rhos per feature
+%   - ps = struct with p values per feature
+%
+% Author: Ruchella Kock, Leiden University, 2024
+%
+arguments
+    features;
+    options.n_delays double = 5;
+    options.n_chans double = 64;
+end
+%% repeat the calculation for 2 features 
 for feature={'amplitude', 'density'}
-    % prepare data
-    n_delays = 5;
-    rho = nan(64,(n_delays*2)+1);
-    p = nan(64,(n_delays*2)+1);
+    % delay in minutes
+    % delays prior to and after (n_delays*2) including no delay (+1)
+    rho = nan(options.n_chans,(options.n_delays*2)+1);
+    p = nan(options.n_chans,(options.n_delays*2)+1);
     
-    for delay=1:(n_delays*2)+1
+    % repeat the correlation with shifted data 
+    for delay=1:(options.n_delays*2)+1
         % 1 to 5 is delay after
-        if delay <= n_delays
+        if delay <= options.n_delays
             tmp_feature = zeros(size(features.(feature{:})));
             tmp_feature(:,1:size(tmp_feature,2)-delay+1) = features.(feature{:})(:,delay:end);
-        elseif delay > n_delays+1
+        elseif delay > options.n_delays+1
         % 6 to end is delay before
             tmp_feature = zeros(size(features.(feature{:})));
             tmp_feature(:,1+delay:end) = features.(feature{:})(:,1:end-delay);
@@ -18,11 +43,11 @@ for feature={'amplitude', 'density'}
             tmp_feature = features.(feature{:});
         end
         % calculte the correlation per electrode 
-        for chan=1:64
+        for chan=1:options.n_chans
             [rho(chan,delay),p(chan,delay)] = corr(features.rate(chan,:)',tmp_feature(chan,:)', 'Type', 'Spearman', 'rows','complete');
         end
     end
-    % assign results 
+    % keep results per feature
     rhos.(feature{:}) = rho;
     ps.(feature{:}) = p;
 end
--- src/sw_to_behavior/prepare_features_rate.m
+++ src/sw_to_behavior/prepare_features_rate.m
@@ -1,27 +1,57 @@
-function [features] = prepare_features_rate(res, options)
+function [features] = prepare_features_rate(res, pp,options)
+%% calculate behavioral rate and slow wave features (amplitude and density)
+%
+% **Usage:** [features] = rate_sws_features(res,pp)
+%                       = rate_sws_features(...,'time_win', 30000)
+%
+% Input(s):
+%   - res = processed slow waves data (from sw_to_behavior_all_pps)
+%   - pp [int] = pp number
+%
+% Optional Input(s)
+%   - plot (Default: 0) = logical 1 plot results, 0 otherwise
+%   - time_win (Default: 60000) = duration of the timewindow in millisecond
+%   - n_chans (Default: 64) = number of electrodes
+%
+% Output(s):
+%   - features = Struct with following fields (Shape : number electrodes x number of time windows)
+%       density = slow waves frequency over time_win duration 
+%       amplitude = median slow waves amplitude over time_win duration 
+%       rate = number of taps over time_win duration 
+%
+% Author: Ruchella Kock, Leiden University, 2024
+%
 arguments
     res;
+    pp;
     options.plot logical = 0;
     options.time_win = 60000;
     options.n_chans = 64;
 end
+% initialize variables
 density = nan(options.n_chans,floor((res(pp).taps(end)-res(pp).taps(1))/60000));
 rate = nan(options.n_chans,floor((res(pp).taps(end)-res(pp).taps(1))/60000));
 amplitude = nan(options.n_chans,floor((res(pp).taps(end)-res(pp).taps(1))/60000));
 
+% calculate features per electrode
 for chan =1:options.n_chans
     count = 1;
+    % slow waves during smartphone behavior at most negative peak index
     slow_waves = [res(pp).behavior_sws{chan,:}];
+    % index of smartphone taps
     taps = [res(pp).taps];
+    % select slow wave amplitudes
     neg_amplitudes = [res(pp).refilter.channels(chan).maxnegpkamp{:}];
     pos_amplitudes = [res(pp).refilter.channels(chan).maxpospkamp{:}];
 
-    for i=res(pp).taps(1):options.time_win:floor((res(pp).taps(end)-res(pp).taps(1))/60000)*60000+res(pp).taps(1)
+    for i=res(pp).taps(1):options.time_win:floor((res(pp).taps(end)-res(pp).taps(1))/options.time_win)*options.time_win+res(pp).taps(1)
+        % select slow waves within time window 
         windowed_sws = slow_waves(slow_waves>=i & slow_waves<=i+options.time_win);
         neg = neg_amplitudes(slow_waves>=i & slow_waves<=i+options.time_win);
         pos = pos_amplitudes(slow_waves>=i & slow_waves<=i+options.time_win);
         amplitudes = abs(neg-pos);
         rate(chan,count) = length(taps(taps>=i & taps<=i+options.time_win));
+        % check if there were any events 
         if ~isempty(windowed_sws)
             density(chan,count) = length(windowed_sws);
             amplitude(chan,count) = median(amplitudes);
@@ -29,9 +59,11 @@ for chan =1:options.n_chans
         count = count +1;
     end
 end
+% save results in a struct 
 features.density = density;
 features.amplitude = amplitude;
-%%
+features.rate = rate;
+%% plot results , density and rate
 if options.plot
     h = figure;
     n=4;
